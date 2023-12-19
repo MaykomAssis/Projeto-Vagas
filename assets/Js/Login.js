24c2130a5b6b91060a2048009a4a45a922205f06
Maykom De Assis <mayassiskon@gmail.com>
2023-12-19 14:55:34 -0300

    .

--- assets/Js/Login.js
+++ assets/Js/Login.js
@@ -1,66 +1,25 @@
-/*const usuarioPermitidoAdmin = "admin";
-const usuarioPermitidoUser = "user";
-
 function logar() {
     var matricula = document.getElementById('Matricula').value;
     var senha = document.getElementById('Senha').value;
-
-    if ((matricula === usuarioPermitidoAdmin && senha === usuarioPermitidoAdmin) ||
-        (matricula === usuarioPermitidoUser && senha === usuarioPermitidoUser)) {
-        alert('Login Bem-Sucedido!');
-        location.href = "Vagas.html";
-    } else {
-        alert('Matrícula ou senha incorretos');
-    }
-
-    function exibirLinkAdmin() {
-        const linkAdmin = document.getElementById('linkAdmin');
-        if (linkAdmin) {
-            linkAdmin.classList.remove('hidden');
-        }
-} */
-
-
- /*function logar() {
-
-    var Matricula = document.getElementById('Matricula').value;
-    var Senha = document.getElementById('Senha').value;
-
-    if (Matricula === 'admin' && Senha === 'admin') {
-        alert('Login como admin bem-sucedido!');
-        location.href = "Vagas.html";
-    } else if (Matricula === 'user' && Senha === 'user') {
-        alert('Login como user bem-sucedido!');
-        location.href = "Vagas.html";
-        exibirLinkAdmin();
-    } else {
-        alert('Matrícula ou senha incorretos');
-    }
-}
-
-function exibirLinkAdmin() {
-    const linkAdmin = document.getElementById('linkAdmin');
-    if (linkAdmin) {
-        linkAdmin.classList.remove('hidden');
-    }
-}*/
-
- function logar() {
-    var matricula = document.getElementById('Matricula').value;
-    var senha = document.getElementById('Senha').value;
+    
 
     if (matricula === 'admin' && senha === 'admin') {
         alert('Login como admin bem-sucedido!');
         location.href = "Vagas.html";
+        localStorage.setItem('admin', matricula && senha)
+
+
     } else if (matricula === 'user' && senha === 'user') {
         alert('Login como user bem-sucedido!');
         location.href = "Vagas.html";
+        localStorage.setItem('admin' , matricula && senha)
+
+
     } else {
         alert('Matrícula ou senha incorretos');
     }
 }
 
-// Chama a função exibirLinkAdmin() apenas se o usuário for "admin"
 function exibirLinkAdmin() {
     const matricula = document.getElementById('Matricula').value;
     if (matricula === 'admin' && senha === 'admin') {
@@ -70,22 +29,6 @@ function exibirLinkAdmin() {
         }
     }
 } 
-
-/*function logar() {
-    var matricula = document.getElementById('Matricula').value;
-    var senha = document.getElementById('Senha').value;
-
-    if (matricula === 'admin' && senha === 'admin') {
-        alert('Login como admin bem-sucedido!');
-        location.href = "Vagas.html";
-    } if (matricula === 'user' && senha === 'user') {
-        alert('Login como user bem-sucedido!');
-        location.href = "Vagas.html";
-       
-    } else {matricula != 'admin' && senha != 'admin' || matricula != 'user' && senha != 'user'} {
-        alert('Matrícula ou senha incorretos');
-    }
-}*/
 
 
 
