34637be3a98464b74cf9d437564725ae64a3cf54
Dredeco <andrefersouza@gmail.com>
2023-12-22 14:49:50 -0300

    fix: configurando as funções

--- assets/Js/Vagas-editar.js
+++ assets/Js/Vagas-editar.js
@@ -1,34 +1,25 @@
 const value__limitButton = document.getElementById('value__limit');
 const plusButton = document.getElementById('plus');
-const menosButton = document.getElementById('menos');
-const limitButton = document.getElementById('limit');
+const menosButton = document.getElementById('menos');;
 
 const updateValue = () => {
-    value__limitButton.innerHTML = count;
+    value__limitButton.innerHTML = limit;
 };
 
-let count = 0;
-
-let contador2 = parseInt(localStorage.getItem('count')) || 0;
+let contador2 = parseInt(localStorage.getItem('limit')) || 0;
+let limit = contador2;
 
 document.getElementById('value__limit').innerHTML = contador2
 
 plusButton.addEventListener('click', () => {
-    count += 1;
-
-    localStorage.setItem('count', count);
-
+    limit += 1;
+    localStorage.setItem('limit', limit);
     updateValue();
 });
 
 menosButton.addEventListener('click', () => {
-    count -= 1;
-
-    localStorage.setItem('count', count);
-
+    limit -= 1;
+    localStorage.setItem('limit', limit);
     updateValue();
 });
-
-document.addEventListener('mouseup', () => clearInterval(intervalid));
-
 
--- assets/Js/Vagas.js
+++ assets/Js/Vagas.js
@@ -1,53 +1,46 @@
 const value = document.getElementById('value');
 const ocuparButton = document.getElementById ('ocupar');
 const liberarButton = document.getElementById ('liberar');
+const limitValue = document.getElementById("limit")
 
-const updateValue = () => {
-    value.innerHTML = count;
-};
-
-let count = 0;
-
-let intervalid = 0;
-
+var count = 0;
 let contador = parseInt(localStorage.getItem('count')) || 0;
+let contadorlimit = parseInt(localStorage.getItem('limit')) || 0;
 
-document.getElementById('value').innerText = contador;
+
+limitValue.innerHTML = parseInt(contadorlimit)
+value.innerHTML = contador;
 
 ocuparButton.addEventListener('click', () => {
-        count += 1;
+    count += 1;
+    localStorage.setItem('count', count);
 
-        localStorage.setItem('count', count);
-
-        if (count >= 1){
-
-            ocuparButton.disabled = true;
-        }
-
-            liberarButton.disabled = false;
-
-        updateValue();
-    }, 100);
+    if (count >= 1){
+        ocuparButton.disabled = true;
+    } {
+        liberarButton.disabled = false;
+    }
+        updateValue2();
+    });
 
 liberarButton.addEventListener('click', () => {
     if (count == 0) {
-
         
     } else {count -= 1} 
-        
         localStorage.setItem('count', count);
 
-        if (count <= 1) {
+    if (count <= 1) {
+        ocuparButton.disabled = null;
+    } {
+        liberarButton.disabled = true;
+    }
+        updateValue2();
+});
 
-            ocuparButton.disabled = null;
-        }
-            liberarButton.disabled = true;
 
-        updateValue();
-
-    }, 100);
-
-document.addEventListener('mouseup', () => clearInterval(intervalid)); 
+const updateValue2 = () => {
+    value.innerHTML = count;
+};
 
 
 
